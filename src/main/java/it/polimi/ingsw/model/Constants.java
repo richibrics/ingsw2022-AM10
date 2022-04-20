7555afe0059c024a2a4dcea69696628afd3efe12
Riccardo Briccola <riccardo.briccola@mail.polimi.it>
2022-04-20 16:42:56 +0200

    Constants class for model

---
+++ src/main/java/it/polimi/ingsw/model/Constants.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.model;
+
+public class Constants {
+    public static int ACTION_ID_SETUP = -1;
+    public static int ACTION_ID_ON_SELECTION_OF_WIZARD = 0;
+    public static int ACTION_ID_DRAW_FROM_BAG_TO_CLOUD = 1;
+    public static int ACTION_ID_ON_SELECTION_OF_ASSISTANTS_CARD = 2;
+    public static int ACTION_ID_ON_SELECTION_OF_CHARACTER_CARD = 3;
+    public static int ACTION_ID_MOVE_STUDENTS_FROM_ENTRANCE = 4;
+    public static int ACTION_ID_MOVE_MOTHER_NATURE = 5;
+    public static int ACTION_ID_FROM_CLOUD_TILE_TO_ENTRANCE = 6;
+    public static int ACTION_ID_CALCULATE_INFLUENCE = 7;
+    public static int ACTION_ID_ASSIGN_PROFESSORS = 7;
+    public static int ACTION_ID_CHECK_END_MATCH_CONDITION = 9;
+    public static int ACTION_ID_MAILMAN = 10;
+    public static int ACTION_ID_AMBASSADOR = 11;
+    public static int ACTION_ID_FRIAR = 12;
+    public static int ACTION_ID_JESTER = 13;
+    public static int ACTION_ID_MINSTREL = 14;
+    public static int ACTION_ID_LADY = 15;
+    public static int ACTION_ID_THIEF = 16;
+    public static int ACTION_ID_HERBALIST = 17;
+}
