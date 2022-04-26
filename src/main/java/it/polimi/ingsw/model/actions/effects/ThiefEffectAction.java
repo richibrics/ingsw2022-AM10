55d72786589fe473aba13e8d0c6e6dedfb164ca1
LeonardoGiovanniCavallini <leonardogiovanni.cavallini@mail.polimi.it>
2022-04-26 15:13:48 +0200

    ThiefEffect update

--- src/main/java/it/polimi/ingsw/model/actions/effects/ThiefEffectAction.java
+++ src/main/java/it/polimi/ingsw/model/actions/effects/ThiefEffectAction.java
@@ -5,11 +5,15 @@ import it.polimi.ingsw.controller.exceptions.WrongMessageContentException;
 import it.polimi.ingsw.model.ModelConstants;
 import it.polimi.ingsw.model.actions.Action;
 import it.polimi.ingsw.model.game_components.PawnColor;
+import it.polimi.ingsw.model.game_components.StudentDisc;
+import it.polimi.ingsw.model.managers.CommonManager;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class ThiefEffectAction extends Action {
     private PawnColor color;
+
     public ThiefEffectAction(GameEngine gameEngine) {
         super(ModelConstants.ACTION_THIEF_ID, gameEngine);
     }
@@ -40,6 +44,14 @@ public class ThiefEffectAction extends Action {
 
     @Override
     public void act() throws Exception {
-
+        ArrayList<StudentDisc> studentsToPush = new ArrayList<>();
+        for (int i = 1; i <= getGameEngine().getNumberOfPlayers(); i++) {
+            ArrayList<StudentDisc> table = CommonManager.takeSchoolBoardByPlayerId(getGameEngine(), i).getDiningRoomColor(this.color);
+            for (int studentPosition = table.size() - 1; studentPosition >= table.size() - 3 && studentPosition >= 0; studentPosition--) {
+                CommonManager.takeSchoolBoardByPlayerId(getGameEngine(), i).removeStudentFromDiningRoom(table.get(studentPosition));
+                studentsToPush.add(table.get(studentPosition));
+            }
+        }
+        this.getGameEngine().getTable().getBag().pushStudents(studentsToPush);
     }
 }
