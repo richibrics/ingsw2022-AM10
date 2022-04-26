8026757d6ea14db16c4b1c260529828f78fb8f69
marcoCalzavara <marco2.calzavara@mail.polimi.it>
2022-04-26 13:31:59 +0200

    Tests effects

---
+++ src/test/java/it/polimi/ingsw/model/actions/effects/TestMailmanEffectAction.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.model.actions.effects;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestMailmanEffectAction {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setOptions() {
+    }
+
+    @Test
+    void modifyRoundAndActionList() {
+    }
+
+    @Test
+    void act() {
+    }
+}
