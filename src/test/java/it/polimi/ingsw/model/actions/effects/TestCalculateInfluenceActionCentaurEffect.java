cb0b2f87bfdbdc7b23b09af7d48c356dd5fd2841
Riccardo Briccola <riccardo.briccola@mail.polimi.it>
2022-04-28 16:29:49 +0200

    Fixed JUnit5 imports in effects

--- src/test/java/it/polimi/ingsw/model/actions/effects/TestCalculateInfluenceActionCentaurEffect.java
+++ src/test/java/it/polimi/ingsw/model/actions/effects/TestCalculateInfluenceActionCentaurEffect.java
@@ -19,8 +19,7 @@ import org.junit.jupiter.api.Test;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class TestCalculateInfluenceActionCentaurEffect {
 
