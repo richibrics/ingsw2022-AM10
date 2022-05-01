5e9b20f3f4c81e3616ec75db0360eda7d41acce7
Riccardo Briccola <riccardo.briccola@mail.polimi.it>
2022-05-01 21:18:47 +0200

    ServerClientConnection prototype

---
+++ src/main/java/it/polimi/ingsw/network/server/ServerClientConnection.java
@@ -0,0 +1,58 @@
+package it.polimi.ingsw.network.server;
+
+import it.polimi.ingsw.controller.User;
+import it.polimi.ingsw.network.NetworkConstants;
+
+import java.net.Socket;
+
+public class ServerClientConnection implements Runnable {
+    private Socket clientSocket;
+    private User user;
+    private int stillAliveTimer;
+
+    public ServerClientConnection(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+        this.user = null;
+        this.resetTimer();
+    }
+
+    /**
+     * Main method of the ServerClientConnection thread that receives Clients messages and sort them.
+     * Don't call this method directly but use start() instead.
+     */
+    @Override
+    public void run() {
+
+    }
+
+    private void addClientToLobby() {
+
+    }
+
+    private void requestUser() {
+
+    }
+
+    public void sendMessage(String message) {
+
+    }
+
+    public void receiveMessage() {
+
+    }
+
+    public void closeConnection() {
+
+    }
+
+    public void deserialize() {
+
+    }
+
+    /**
+     * Resets Still Alive timer to its initial value.
+     */
+    public void resetTimer() {
+        this.stillAliveTimer = NetworkConstants.INITIAL_STILL_ALIVE_TIMER_VALUE;
+    }
+}
